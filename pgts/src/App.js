179065e7aa45196bf5b7dd82f3684af52827b7b2
Mikazuchi <taingmat@uw.edu>
2019-12-31 18:25:01 -0800

    added chewtle to react app

--- pgts/src/App.js
+++ pgts/src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"./chewtle.png"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>the chewtle</code> and save to reload.
         </p>
         <a
           className="App-link"
